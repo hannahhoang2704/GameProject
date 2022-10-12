8ee6f90f5db482d3db59ebcb92bb62dc5b4a6846
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-10-12 15:57:03 +0300

    Typo, text editing
    - avoid listed ICAO multiple times
    - shortern script
    -typo

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-import sys
 from time import sleep
 import mysql.connector
 import random
 from geopy import distance
-import time,os,sys
+import time, os, sys
 
 connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port=3306,
-         database='game_project',
-         user='root',
-         password='MyN3wP4ssw0rd',                              #CHECK PASSWORD TO RUN THE PROGRAM!!!
-         autocommit=True
-         )
+    host='127.0.0.1',
+    port=3306,
+    database='game_project',
+    user='root',
+    password='MyN3wP4ssw0rd',  # CHECK PASSWORD TO RUN THE PROGRAM!!!
+    autocommit=True
+)
 
 # Questionaires and answers
-questions = ("Wasting less food is a way to reduce greenhouse gas emissions.",  # 0
-             "The overwhelming majority of scientists agree that climate change is real and caused by humans.",  # 1
-             "Combustion removes carbon from the atmosphere",  # 2
-             "Unplugging your electronics when you’re not using them could shave as much as 10 percent off your energy"
-             "bill.",  # 3
-             "Climate change is heating the world evenly.",  # 4
-             "Climate change and extreme weather are linked.",  # 5
-             "As climate warms, we will no longer have snow storms and cold days.",  # 6
-             "We definitely know that tornadoes are increasing in frequency because of climate change.",  # 7
-             "All climate scientists in the 1970s were saying that we were going into an Ice Age or cooler Earth.",  # 8
-             "Growing leafy green plants is the most effective method for permanently storing carbon dioxide.",  # 9
-             "Scientists have reached common agreement and have adopted consensus-driven global policies that monitor"
-             "effective, safe, reliable long-term storage of carbon dioxide.",  # 10
-             "The atmosphere is composed mainly of nitrogen and oxygen.",  # 11
-             "Climate change is the same thing as global warming",  # 12
-             "The Earth's has climate changed before",  # 13
-             "Climate change can harm plants and animals",  # 14
-             "The sun causes global warming")  # 15
+questions = (
+"Wasting less food is a way to reduce greenhouse gas emissions.",  # 0
+"The overwhelming majority of scientists agree that climate change is real and caused by humans.",
+# 1
+"Combustion removes carbon from the atmosphere",  # 2
+"Unplugging your electronics when you’re not using them could shave as much as 10 percent off your energy"
+"bill.",  # 3
+"Climate change is heating the world evenly.",  # 4
+"Climate change and extreme weather are linked.",  # 5
+"As climate warms, we will no longer have snow storms and cold days.",  # 6
+"We definitely know that tornadoes are increasing in frequency because of climate change.",
+# 7
+"All climate scientists in the 1970s were saying that we were going into an Ice Age or cooler Earth.",
+# 8
+"Growing leafy green plants is the most effective method for permanently storing carbon dioxide.",
+# 9
+"Scientists have reached common agreement and have adopted consensus-driven global policies that monitor"
+"effective, safe, reliable long-term storage of carbon dioxide.",  # 10
+"The atmosphere is composed mainly of nitrogen and oxygen.",  # 11
+"Climate change is the same thing as global warming",  # 12
+"The Earth's climate has changed before",  # 13
+"Climate change can harm plants and animals",  # 14
+"The sun causes global warming")  # 15
 answers = ("true",  # 0
            "true",  # 1,
            "false",  # 2
@@ -52,10 +56,11 @@ answers = ("true",  # 0
 
 
 ##ALL FUNCTIONS
-#Function for underlying the text
+# Function for underlying the text
 class Format:
     end = '\033[0m'
     underline = '\033[4m'
+
 
 # Function 1: for text effect
 def text_effect(text):
@@ -63,14 +68,17 @@ def text_effect(text):
         sleep(0.05)
         sys.stdout.write(char)
 
+
 def typingPrint(text):
-  for character in text:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.03)
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
 
 def clearScreen():
-  os.system("clear")
+    os.system("clear")
+
 
 # Function 2: check if the city exists in database
 
@@ -89,10 +97,11 @@ def check_city(city):
 
 # Function 3: search airports in the city
 
+icao_list = []
 def municipality_search(city):
     sql = "SELECT ident, name FROM airport"
     sql += " WHERE municipality='" + city + "'"
-    icao_list = []
+    #icao_list = []
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -100,8 +109,8 @@ def municipality_search(city):
         for row in result:
             print(f"     ICAO code: {row[0]}, Airport name: {row[1]}")
             icao_list.append(row[0])
-            #print(icao_list)
     return icao_list
+
 
 
 # Function 4: call the airport in the chosen city
@@ -146,7 +155,8 @@ def weather(correct_answer):
     cursor.execute(sql, ran)
     result = cursor.fetchall()
     for i in result:
-        print(f"{Format.underline + 'Weather forecast:' + Format.end} {i[1]}.\n")
+        print(
+            f"{Format.underline + 'Weather forecast:' + Format.end} {i[1]}.\n")
         text_effect(f"That's why your Co2 will change by {i[0]} units.\n")
         co2_score = int(i[0])
     return co2_score
@@ -166,7 +176,7 @@ def calc_distance_to_Rov(position, distance):
 intro1 = f"Each year children from all over the world\nfly to {Format.underline + 'Rovaniemi' + Format.end} to meet Santa.\nBelievers to hug him and non-believers to expose him by pulling his beard. \n"
 text_effect(intro1)
 
-intro2 = f"\nOn your way to Rovaniemi you will come across different challenges.\nOne of them is flight's high {Format.underline + 'Co2 consumption.' + Format.end}\n{Format.underline + 'Your main challenge is to keep Co2 consumption as low as possible.' + Format.end}\nMake sure it doesn't go over 10 000, otherwise it is GAME OVER.\n"
+intro2 = f"\nOn your way to Rovaniemi you will come across different challenges.\nOne of them is flight's {Format.underline + 'Co2 consumption.' + Format.end}\nYour main goal is to {Format.underline + 'keep Co2 consumption as low as possible.' + Format.end}\nMake sure it doesn't go over 10 000 units!\n"
 text_effect(intro2)
 typingPrint("Your mission starts in 3..")
 time.sleep(1)
@@ -176,7 +186,7 @@ typingPrint("1..")
 time.sleep(1)
 clearScreen()
 clearScreen()
-intro3 = "First things first, do you believe in Santa Clause? "
+intro3 = "Firstly, do you believe in Santa Claus? "
 while True:
     start = input(intro3)
     start = start.lower()
@@ -184,35 +194,39 @@ while True:
         text_effect("\nMe neither. Get ready for an adventure.")
         break
     elif start == "yes":
-        text_effect("\n***It's going to be an eye-opening experience for you. Let's go!***\n")
+        text_effect(
+            "\n***It's going to be an eye-opening experience for you. Let's go!***\n")
         break
     else:
-        print("Just type yes or no.")
+        print("Just type yes or no. ")
 
-text_effect("Lucky for you, there are other like-minded people out there. \n")
+text_effect(" Lucky for you, there are other like-minded people out there. \n")
 
 friends = input("Do you want to meet them? ")
 friends = friends.lower()
 
 while friends != "yes" or friends != "no":
     if friends == "yes":
-        text_effect(f"\nI thought so.\nYour new friends care about our planet Earth.\nIn order to have them join you on your quest they will have {Format.underline + 'questions' + Format.end} for you.\n"
-          f"Answer them correctly and you won't fly alone, plus you will have a good chance to stay within the Co2 budget.\n")
+        text_effect(
+            f"\nI thought so.\nIn order to have them join you on your quest they will have {Format.underline + 'questions' + Format.end} for you.\n"
+            f"Answer them correctly and you will save some C02!\n")
         break
     elif friends == "no":
-        text_effect("\nIf you don't care about meeting new friends, I hope that you care about planet Earth.\n"
-          f"On your trip you will come across different {Format.underline + 'questions.' + Format.end}\nAnswer correctly and you might save some {Format.underline + 'Co2' + Format.end} and reach your destination within co2 budget.\n")
+        text_effect(
+            "\nIf you don't care about meeting new friends, try to save some C02 during your flight\nby answering "
+            f"{Format.underline + 'questions' + Format.end} correctly and {Format.underline + 'keep Co2 within the budget.' + Format.end}\n")
         break
     friends = input("Just type yes or no!")
     friends = friends.lower()
 
-typingPrint(f"{Format.underline + 'Your current Co2 consumption : 5000'+ Format.end}\n")
+typingPrint(f"'Before reaching the 1st destination, your C02 is {Format.underline + '5000 units.' + Format.end}\n")
 time.sleep(3)
 clearScreen()
 
 # User input name and city
 
-player_name = input("Lets start with your name: ")
+player_name = input("Lets start with your name: ").capitalize()
+
 
 municipality = input("From which city you want to start your journey: ")
 print("\nHere are your adventure starting point options: ")
@@ -224,13 +238,13 @@ else:
 
 # Users choose the airport in the chosen city
 
-icao_selection = input("You can see all the airports in your city above.\nChoose an airport you want to travel from by enter ICAO:   ")
+icao_selection = input("You can see all the airports in your city above.\nEnter ICAO to choose the airport you want to fly:   ")
 
-while icao_selection not in municipality_search(municipality):
+while icao_selection.upper() not in icao_list:
     print("Oops! Check again the ICAO code. You can't arrive to the airport if you don't call ICAO code correctly!")
     icao_selection = str(input("Enter ICAO code again: "))
 else:
-    location = icao_selection                         # store value of icao
+    location = icao_selection.upper() # store value of icao
     airport_name = call_airport(icao_selection)  # store airport name in a variable
 
 # The distance between the chosen airport to Rovaniemi airport
@@ -238,7 +252,8 @@ else:
 a = airport_position(icao_selection)
 b = airport_position("EFRO")
 dist = distance.distance(a, b).km
-print(f"\n{Format.underline + f'Distance between {airport_name} and Rovaniemi Airport is {dist:.2f}km.' + Format.end}\n")
+print(
+    f"\n{Format.underline + f'Distance between {airport_name} and Rovaniemi Airport is {dist:.2f}km.' + Format.end}\n")
 typingPrint("Take-off in 3..")
 time.sleep(1)
 typingPrint("2..")
@@ -248,70 +263,76 @@ time.sleep(1)
 clearScreen()
 clearScreen()
 
-
 # PHASE 2: GAME START!
 
 co2_budget = 10000
 co2_consumed = 5000
-destinations = 1           
+destinations = 1
 
 questions = list(questions)
 answers = list(answers)
 
 while co2_consumed < co2_budget and destinations <= 5:
     time.sleep(2)
-    text_effect(f"\nYou are approaching stop number: {destinations}. Get your thinking hat on and answer the question:\n")
+    text_effect(
+        f"\nYou are approaching destination {destinations}. Answer the question:\n")
     # ask question & get point by answer
     random_index_number = random.randint(0, len(questions) - 1)
     user_answer = "0"
     while user_answer != "true" and user_answer != "false":
-        print(questions[random_index_number])                  #call random question in questions list
+        print(questions[
+                  random_index_number])  # call random question in questions list
         user_answer = input("True or false: ")
         time.sleep(2)
         user_answer = user_answer.lower()
 
     right_answer = answers[random_index_number]
-    questions.pop(random_index_number)                         #pop the question from the list to avoid duplicated question in next destination
+    questions.pop(
+        random_index_number)  # pop the question from the list to avoid duplicated question in next destination
     answers.pop(random_index_number)
 
-#Check if players answer correct or not
+    # Check if players answer correct or not
 
     if user_answer == right_answer:
-        print(f"Good job! That was the {Format.underline + 'correct' + Format.end} answer.")
-        text_effect(f"{player_name}, look ahead, it looks like the weather is in your favor.\n")
+        print(
+            f"Good job! That was the {Format.underline + 'correct' + Format.end} answer.")
+        text_effect(
+            f"{player_name}, look ahead, it looks like the weather is in your favor.\n")
         co2_consumed += weather(True)
-       
+
     elif user_answer != right_answer:
         print(f"You answered {Format.underline + 'incorrectly.' + Format.end}")
         text_effect(
-            f"{player_name} not only you didn't answer correctly but now it seems that the weather has changed.\n")
+            f"{player_name}, not only you didn't answer correctly but also the weather has changed.\n")
         co2_consumed += weather(False)
-     
-    text_effect(f"{Format.underline + 'Your current Co2 level is:' + Format.end} {co2_consumed} units\n")            #inform players their co2 score
-    print(f"You are {calc_distance_to_Rov(destinations, dist)} km away from Rovaniemi.")                             # Inform players how far from that destination to Rovaniemi
-    
+
+    text_effect(
+        f"{Format.underline + 'Your current Co2 is:' + Format.end} {co2_consumed} units\n")  # inform players their co2 score
+    print(
+        f"You are {calc_distance_to_Rov(destinations, dist)} km away from Rovaniemi.")  # Inform players how far from that destination to Rovaniemi
+
     destinations += 1
-     
+
 else:
-    #Check if players win or lost the game
+    # Check if players win or lost the game
     if co2_consumed >= co2_budget:
         text_effect(
-            f"Oh, no! Your Co2 consumption level was {co2_consumed} units. This exceeds your budget. Game over! Try again soon - Santa won't wait forever.")
+            f"Oh, no! Your Co2 consumption level was {co2_consumed} units. This exceeds your budget.\nGAME OVER! Try again soon - Santa won't wait forever.")
     else:
         text_effect(
-            f"Congratulations, your flight is about to land with {co2_consumed} Co2 units consumed. You passed all the challenges and now Santa awaits.")
+            f"Congratulations! YOU WIN!\nYour flight is about to land with {co2_consumed} Co2 units consumed. You passed all the challenges and now Santa awaits.")
 
-#Store the player score in the database
+
+# Store the player score in the database
 
 def record_score(co2_consumed, co2_budget, location, screen_name):
     sql = "insert into game(co2_consumed, co2_budget, location, screen_name) values(" \
-          + str(co2_consumed) + "," + str(co2_budget) + ", '" + str(location) + "', '" + str(screen_name) + "')"
-    #print(sql)
+          + str(co2_consumed) + "," + str(co2_budget) + ", '" + str(
+        location) + "', '" + str(screen_name) + "')"
+    # print(sql)
     cursor = connection.cursor()
     cursor.execute(sql)
-    #if cursor.rowcount == 1:
-        #print("Data is inserted")
+    # if cursor.rowcount == 1:
+    # print("Data is inserted")
 
 record_score(co2_consumed, co2_budget, location, player_name)
-
-
